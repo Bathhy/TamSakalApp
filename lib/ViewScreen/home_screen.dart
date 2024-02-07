47566312abaf76bb5a81b546a58b850334f2730a
keosambath <keosambathnov@gmail.com>
2024-02-07 11:59:31 +0700

    add drawer

--- lib/ViewScreen/home_screen.dart
+++ lib/ViewScreen/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:practiceloginlayout/RepoUni/repo_uni.dart';
 import 'package:practiceloginlayout/colo_const/color_const.dart';
 import 'package:practiceloginlayout/controller/page_control.dart';
-import 'package:practiceloginlayout/splashscreen/declareimage.dart';
 import 'package:get/get.dart';
 
 class Homescreen extends StatefulWidget {
@@ -37,11 +35,13 @@ class _HomescreenState extends State<Homescreen> {
           IconButton(
               onPressed: () {},
               icon: Icon(
-                Icons.share,
+                Icons.notifications,
                 color: Colors.white,
               ))
         ],
+        iconTheme: IconThemeData(color: Colors.white),
       ),
+      drawer: drawerMeth(),
       body: ListView(
         children: [
           SizedBox(
@@ -52,20 +52,97 @@ class _HomescreenState extends State<Homescreen> {
                 return Container(
                   height: Get.height,
                   decoration: BoxDecoration(
-                      color: Colors.amber,
-                      image: DecorationImage(
-                          image: AssetImage(_pageview.allList[index].imgUni))),
+                    image: DecorationImage(
+                      image: AssetImage(_pageview.allList[index].imgUni),
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 );
               },
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Text(
             "University Pedia is a website for students who have just graduated from the 12th grade and want to find a university or information about university majors to continue their studies.",
             style: TextStyle(
                 color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget drawerMeth() {
+    return Drawer(
+      backgroundColor: myBlueColor,
+      child: ListView(
+        children: [
+          SizedBox(height: 150),
+          ListTile(
+            iconColor: Colors.white,
+            leading: Icon(Icons.account_circle_outlined),
+            title: const Text(
+              'Profile',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {},
+          ),
+          Divider(color: Colors.white, indent: 60.0, endIndent: 58.0),
+          ListTile(
+            iconColor: Colors.white,
+            leading: Icon(Icons.add_shopping_cart),
+            title: const Text(
+              'Orders',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {},
+          ),
+          Divider(color: Colors.white, indent: 60.0, endIndent: 58.0),
+          ListTile(
+            iconColor: Colors.white,
+            leading: Icon(Icons.sell_outlined),
+            title: const Text(
+              'Offer and Promo',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {},
+          ),
+          Divider(color: Colors.white, indent: 60.0, endIndent: 58.0),
+          ListTile(
+            iconColor: Colors.white,
+            leading: Icon(Icons.sticky_note_2_outlined),
+            title: const Text(
+              'Privacy Policy',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {},
+          ),
+          Divider(color: Colors.white, indent: 60.0, endIndent: 58.0),
+          ListTile(
+            iconColor: Colors.white,
+            leading: Icon(Icons.security_outlined),
+            title: const Text(
+              'Security',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {},
+          ),
+          SizedBox(height: 100),
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_forward_sharp,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Sign-Out',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 17,
+              ),
+            ),
           ),
         ],
       ),
