0e03239d8c01fa30f9dc3a1240d77748cc5d799c
keosambath <keosambathnov@gmail.com>
2024-02-17 22:50:20 +0700

    finish Account setting UI

--- lib/bottom_navi_view/account_view/personal_prof_view.dart
+++ lib/bottom_navi_view/account_view/personal_prof_view.dart
@@ -12,116 +12,229 @@ class PersonalAccount extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          _AppbarProf(),
+          SizedBox(height: 10),
+          _profileNamecard(),
+          SizedBox(height: 20),
+          _personalinforcard()
+        ],
+      ),
+    );
+  }
+
+  Widget _personalinforcard() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
           Container(
-            height: Get.height * 0.14,
+            height: Get.height * 0.50,
+            width: Get.width * 0.9,
             decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: myBlueColor,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20))),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 20),
-                  child: UniText(
-                      label: "Welcome Sam",
-                      color: Colors.white,
-                      fontweight: FontWeight.bold,
-                      fontsize: 30),
-                ),
-              ],
+                borderRadius: BorderRadius.circular(20), color: colorgrey),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      UniText(
+                        label: "Personal Information",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 15),
+                      UniText(
+                        label: "First Name",
+                        fontsize: 11,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "Nov",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 15),
+                      UniText(
+                        label: "Email Address",
+                        fontsize: 11,
+                        fontweight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "iloveyou@gmail.com",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 25),
+                      UniText(
+                        label: "Address",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 15),
+                      UniText(
+                        label: "Country",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "Camboja",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(height: 40),
+                      UniText(
+                        label: "Last Name",
+                        fontsize: 11,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "Messi",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 15),
+                      UniText(
+                        label: "Phone Number",
+                        fontsize: 11,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "0238899",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(height: 60),
+                      UniText(
+                        label: "City/Province",
+                        fontsize: 15,
+                        color: Colors.grey,
+                      ),
+                      UniText(
+                        label: "Phnom Penh",
+                        fontsize: 15,
+                        fontweight: FontWeight.bold,
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 70),
+                        child: TextButton(
+                            onPressed: () {},
+                            child: UniText(
+                              label: "Edit",
+                              color: myBlueColor,
+                              fontsize: 16,
+                            )),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _profileNamecard() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              UniText(label: "Account Setting", fontweight: FontWeight.bold),
+            ],
+          ),
           SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          Row(
+            children: [
+              Container(
+                height: Get.height * 0.12,
+                width: Get.width * 0.9,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), color: colorgrey),
+                child: Row(
                   children: [
-                    UniText(
-                        label: "Account Setting", fontweight: FontWeight.bold),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Container(
-                      height: Get.height * 0.12,
-                      width: Get.width * 0.9,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: colorgrey),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: CircleAvatar(
-                              radius: 30,
-                              backgroundImage: AssetImage(
-                                imagelogic,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 20),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                UniText(
-                                  label: "Leonard",
-                                  fontsize: 25,
-                                  fontweight: FontWeight.bold,
-                                ),
-                                UniText(
-                                  label: "Cambodia",
-                                  fontsize: 18,
-                                  color: Colors.grey,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 70),
-                            child: TextButton(
-                                onPressed: () {},
-                                child: UniText(
-                                  label: "Edit",
-                                  color: myBlueColor,
-                                  fontsize: 16,
-                                )),
-                          )
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage(
+                          imagelogic,
+                        ),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Container(
-                      height: Get.height * 0.16,
-                      width: Get.width * 0.9,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: colorgrey),
-                      child: Row(
+                    SizedBox(width: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           UniText(
-                            label: "Personal Information",
-                            fontsize: 15,
+                            label: "Leonard",
+                            fontsize: 25,
                             fontweight: FontWeight.bold,
-                          )
+                          ),
+                          UniText(
+                            label: "Cambodia",
+                            fontsize: 18,
+                            color: Colors.grey,
+                          ),
                         ],
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 70),
+                      child: TextButton(
+                          onPressed: () {},
+                          child: UniText(
+                            label: "Edit",
+                            color: myBlueColor,
+                            fontsize: 16,
+                          )),
+                    ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _AppbarProf() {
+    return Container(
+      height: Get.height * 0.14,
+      decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: myBlueColor,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20))),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 20),
+            child: UniText(
+                label: "Welcome Sam",
+                color: Colors.white,
+                fontweight: FontWeight.bold,
+                fontsize: 30),
           ),
         ],
       ),
